928bf9bae406ed112fd463fb4f590e2bbefd6125
Hiojam <hiojam2016@gmail.com>
2023-05-30 13:19:29 -0500

    Tasks from 0 to 12

---
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for i in range(9):
+    for z in range(1, 10):
+        if i < z:
+            if str(i) + str(z) == "89":
+                print("89")
+            print("{}{}".format(i, z), end=", ")
