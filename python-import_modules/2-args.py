81c4672810b5f4a1d54f3d97cf42fe43820f6c18
Hiojam <hiojam2016@gmail.com>
2023-06-02 10:11:03 -0500

    Task 2: fixed output

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -7,10 +7,12 @@ if __name__ != "__main__":
 len = len(sys.argv) - 1
 element = 0
 
-if len != 1:
+if len == 0:
     print("{} arguments.".format(len))
-else:
+elif (len == 1):
     print("{} argument:".format(len))
+else:
+    print("{} arguments:".format(len))
 
 if (len > element):
     for element in range(1, len + 1):
