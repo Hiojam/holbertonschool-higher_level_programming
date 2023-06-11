c078c081ca064cb38dcf19cfcecda9b2cad13ddd
Hiojam <hiojam2016@gmail.com>
2023-06-11 10:25:36 -0500

    Task 2: fixed pycodestyle

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-    
     counter = 0
     try:
         for element in range(x):
@@ -9,6 +8,5 @@ def safe_print_list_integers(my_list=[], x=0):
                 counter += 1
     except Exception:
         return
-    
     print()
     return counter
