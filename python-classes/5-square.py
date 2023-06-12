d9d9bc99f7172d7a0e935cbdaa5f672dfea969fd
Hiojam <hiojam2016@gmail.com>
2023-06-12 09:38:39 -0500

    Task 5

---
+++ python-classes/5-square.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+""" Task 5 """
+
+
+class Square:
+    """A class representing a square."""
+
+    def __init__(self, size=0):
+        if (type(size) == int):
+            if (size < 0):
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = size
+        else:
+            raise TypeError("size must be an integer")
+
+    def area(self):
+        """
+        Calculates the area of the square.
+
+        Returns:
+            float: The area of the Square.
+        """
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        """
+        Retrieves the size value
+
+        Returns:
+            int: The size of the Square.
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """ Sets the size of the Square. """
+        if (type(value) == int):
+            if (value < 0):
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = value
+        else:
+            raise TypeError("size must be an integer")
+
+    def my_print(self):
+        """ Print the area Square. """
+        if (self.__size == 0):
+            print("")
+        else:
+            for z in range(0, self.__size):
+                for i in range(0, self.__size):
+                    print("#", end="")
+                print("")
+    pass
