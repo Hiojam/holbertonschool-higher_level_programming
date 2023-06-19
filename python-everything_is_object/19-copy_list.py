155c9a39aee1c4780134afb1fabd64d8978faf9c
Hiojam <hiojam2016@gmail.com>
2023-06-19 11:18:39 -0500

    Tasks from 0 to 28

---
+++ python-everything_is_object/19-copy_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+def copy_list(a_list):
+    """Copies a list
+
+    Args:
+        a_list (list): The list to be copied
+
+    Returns:
+        list: The copy of the original list
+    """
+    return a_list.copy()
