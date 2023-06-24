952cb3cedf8b6bd7e832e90b96c346bbeaf4b8f6
Hiojam <hiojam2016@gmail.com>
2023-06-24 09:58:23 -0500

    Task 12: remastered

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -6,16 +6,15 @@ def pascal_triangle(n):
     if n <= 0:
         return []
 
-    triangle = [[1]]
+    triangle = [[1]]  # Start with the first row
 
     for i in range(1, n):
-        prev_row = triangle[i - 1]
-        curr_row = [1]
-
+        row = [1]  # First element of each row is always 1
         for j in range(1, i):
-            curr_row.append(prev_row[j - 1] + prev_row[j])
-
-        curr_row.append(1)
-        triangle.append(curr_row)
+            # Calculate the sum of the two numbers above
+            sum_above = triangle[i-1][j-1] + triangle[i-1][j]
+            row.append(sum_above)
+        row.append(1)  # Last element of each row is always 1
+        triangle.append(row)
 
     return triangle
