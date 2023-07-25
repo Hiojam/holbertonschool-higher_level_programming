3a391e825d392f1f11d1c09272c98c3bf844b323
Hiojam <hiojam2016@gmail.com>
2023-07-25 11:31:59 -0500

    Re-worked 2, 8, 10, 13 & 16 tasks

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -2,5 +2,9 @@
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
 INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
-GROUP BY tv_shows.title, tv_show_genres.genre_id
+WHERE EXISTS (
+    SELECT 1
+    FROM tv_show_genres AS sg
+    WHERE sg.tv_show_id = tv_shows.id
+)
 ORDER BY tv_shows.title, tv_show_genres.genre_id;
--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,5 +1,6 @@
 -- Lists all the cities of "California" that can be found in the database "hbtn_0d_usa".
 SELECT * 
 FROM cities 
-WHERE state_id = (SELECT id FROM states WHERE name = 'California')
-ORDER BY id;
+JOIN states ON cities.state_id = states.id
+WHERE states.name = 'California'
+ORDER BY cities.id;
